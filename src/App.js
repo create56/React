06fa14b4088ab18a1acfc6e0f83be6044017eab3
DESKTOP-NUOPUIF\ITPS <yky2496@naver,com>
2021-07-22 18:29:40 +0900

    1react 확장

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  let num = 10;
+  return (
+    <div>
+      <h1>Hello World{num}</h1>
+    </div>
+  );
+}
+
+export default App;
